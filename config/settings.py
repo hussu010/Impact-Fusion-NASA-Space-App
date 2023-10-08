c3c0bd4ee536a1422959150525ac15f390b3e0b2
hussu <hussu010@gmail.com>
2023-10-08 09:48:20 +0545

    change domain to tnbswap

--- config/settings.py
+++ config/settings.py
@@ -1,23 +1,13 @@
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(wrte!f_n7^p*!pmr9zvbgqxct(--cl7^6$omvvwa0$b4w-e1+'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.105.62.58', '127.0.0.1', 'nasa-hackathon.merodera.com', 'localhost']
-
-
-# Application definition
+ALLOWED_HOSTS = ['172.105.62.58', '127.0.0.1', 'nasa-hackathon.tnbswap.com', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -63,20 +53,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "data/db.sqlite3",
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,10 +75,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -105,15 +83,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -135,8 +106,8 @@ AUTHENTICATION_BACKENDS = (
 
 DJOSER = {
     'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': [
-        'http://nasa-hackathon.merodera.com', 'http://nasa-hackathon.merodera.com/github', \
-        'http://localhost:8000', 'http://localhost:8000/github'
+        'http://nasa-hackathon.tnbswap.com', 'http://nasa-hackathon.tnbswap.com/github', \
+        'http://localhost:8000', 'http://localhost:8000/github', \
     ]
 }
 
